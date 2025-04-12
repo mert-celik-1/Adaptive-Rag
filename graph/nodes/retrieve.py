45d177d2794ccfe35b1494d8a5688f9fb71c6559
Mert Celik <mertcelik2399@gmail.com>
2025-04-12 11:21:21 +0300

    state implemented

---
+++ graph/nodes/retrieve.py
@@ -0,0 +1,15 @@
+from typing import Any,Dict
+
+from graph.state import GraphState
+from ingestion import retriever
+
+def retrieve(state:GraphState) -> Dict[str,Any] :
+    print("----RETRIEVE----")
+
+    question = state["question"]
+
+    documents = retriever.invoke(question)
+    return {
+        "question": question,
+        "documents": documents,
+    }
